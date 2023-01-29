854af21c1e8e7b90c970626ec9ef1a1390ec059a
zakir <80246097+zakir-code@users.noreply.github.com>
2023-01-29 16:04:03 +0800

    use ts_proto_out

--- gen-proto.sh
+++ gen-proto.sh
@@ -4,36 +4,33 @@ set -o errexit -o nounset -o pipefail
 
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-TS_PROTO_OPTS="esModuleInterop=true,forceLong=long,useOptionals=true,useDate=false"
-
 OUT_DIR="./src"
-
-PLUGIN_PATH="./bin/protoc-gen-ts_proto_yarn_2"
 
 if [ -d "$OUT_DIR/fx" ]; then
   rm -rf "$OUT_DIR/fx"
 fi
 
-perl -pi -e 's|ibc\.core\.client\.v1\.Height\.|.ibc.core.client.v1.Height.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/tx.proto
-perl -pi -e 's|ibc\.applications\.transfer\.v1\.|.ibc.applications.transfer.v1.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/tx.proto
-perl -pi -e 's|ibc\.applications\.transfer\.v1\.|.ibc.applications.transfer.v1.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/query.proto
+#perl -pi -e 's|ibc\.core\.client\.v1\.Height\.|.ibc.core.client.v1.Height.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/tx.proto
+#perl -pi -e 's|ibc\.applications\.transfer\.v1\.|.ibc.applications.transfer.v1.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/tx.proto
+#perl -pi -e 's|ibc\.applications\.transfer\.v1\.|.ibc.applications.transfer.v1.|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/query.proto
 
 proto_dirs=$(find ./fx-core/proto/fx -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+# shellcheck disable=SC2046
 for dir in $proto_dirs; do
   protoc \
-    --plugin="$PLUGIN_PATH" \
-    --ts_proto_yarn_2_out="$OUT_DIR" \
+    --plugin="$(yarn bin protoc-gen-ts_proto)" \
+    --ts_proto_out="$OUT_DIR" \
     --proto_path="./proto" \
     --proto_path="./fx-core/proto" \
     --proto_path="./cosmos-sdk/proto" \
     --proto_path="./cosmos-sdk/third_party/proto" \
     --proto_path="./ibc-go/proto" \
-    --ts_proto_yarn_2_opt="$TS_PROTO_OPTS" \
+    --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages" \
     "./proto/fx/dex/v1/tx.proto" \
+    "./proto/fx/gov/v1/tx.proto" \
     $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
 
-perl -pi -e 's|.ibc.core.client.v1.Height|ibc.core.client.v1.Height|g' ./fx-core/proto/fx/ibc/applications/transfer/v1/tx.proto
 rm -rf "$OUT_DIR/cosmos" "$OUT_DIR/cosmos_proto" "$OUT_DIR/gogoproto" "$OUT_DIR/google" "$OUT_DIR/ibc"
 
 proto_ts_dirs=$(find ./src/fx -path -prune -o -name '*.ts' -print0 | xargs -0 -n1 dirname | sort | uniq)
